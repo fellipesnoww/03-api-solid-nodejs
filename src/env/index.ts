839b37f3616a20b32f083c25fa41aafac085effb
Fellipe <fellipe2.0e@gmail.com>
2024-03-23 16:41:54 -0300

    lint: aplied lint fixes

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,15 +2,15 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333),
+	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+	PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-    console.error('Invalid environment!!!', _env.error.format());
-    throw new Error('Invalid environment!!!');
+	console.error('Invalid environment!!!', _env.error.format());
+	throw new Error('Invalid environment!!!');
 }
 
 export const env = _env.data;
