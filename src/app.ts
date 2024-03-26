a2248b35938e37788bd12099f130e66543459bd9
Fellipe <fellipe2.0e@gmail.com>
2024-03-25 21:17:09 -0300

    feature: create prisma integration with postgres on docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify';
+import { PrismaClient } from "@prisma/client";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    data: {
+        name: "Fellipe Neves",
+        email: "fellipe@email.com"
+    }
+})
